a5c4754ede5e700ec78b4484172630abf53d2fe0
varygin <varygin@arrival.com>
2022-01-02 18:38:35 +0500

    switch RedisConnection to lettuce mod

--- modules/core/src/main/scala/dev/profunktor/redis4cats/connection/RedisConnection.scala
+++ modules/core/src/main/scala/dev/profunktor/redis4cats/connection/RedisConnection.scala
@@ -16,44 +16,44 @@
 
 package dev.profunktor.redis4cats.connection
 
-import cats.{ ApplicativeThrow, MonadThrow }
+import cats.{ApplicativeThrow, MonadThrow}
 import cats.effect.kernel.Async
 import cats.syntax.all._
 import dev.profunktor.redis4cats.data.NodeId
-import dev.profunktor.redis4cats.effect.{ FutureLift, RedisExecutor }
-import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.async.RedisAsyncCommands
-import io.lettuce.core.api.sync.{ RedisCommands => RedisSyncCommands }
-import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
-import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands
-import io.lettuce.core.cluster.api.sync.{ RedisClusterCommands => RedisClusterSyncCommands }
+import dev.profunktor.redis4cats.effect.{FutureLift, RedisExecutor}
 import scala.util.control.NoStackTrace
+import com.redis.lettucemod.api.async.RedisModulesAsyncCommands
+import com.redis.lettucemod.api.sync.{RedisModulesCommands => RedisModulesSyncCommands}
+import com.redis.lettucemod.api.StatefulRedisModulesConnection
+import com.redis.lettucemod.cluster.api.StatefulRedisModulesClusterConnection
+import com.redis.lettucemod.cluster.api.async.RedisModulesClusterAsyncCommands
+import com.redis.lettucemod.cluster.api.sync.{RedisModulesClusterCommands => RedisModulesClusterSyncCommands}
 
 case class OperationNotSupported(value: String) extends NoStackTrace {
-  override def toString(): String = s"OperationNotSupported($value)"
+  override def toString: String = s"OperationNotSupported($value)"
 }
 
 private[redis4cats] trait RedisConnection[F[_], K, V] {
-  def sync: F[RedisSyncCommands[K, V]]
-  def clusterSync: F[RedisClusterSyncCommands[K, V]]
-  def async: F[RedisAsyncCommands[K, V]]
-  def clusterAsync: F[RedisClusterAsyncCommands[K, V]]
+  def sync: F[RedisModulesSyncCommands[K, V]]
+  def clusterSync: F[RedisModulesClusterSyncCommands[K, V]]
+  def async: F[RedisModulesAsyncCommands[K, V]]
+  def clusterAsync: F[RedisModulesClusterAsyncCommands[K, V]]
   def close: F[Unit]
-  def byNode(nodeId: NodeId): F[RedisAsyncCommands[K, V]]
+  def byNode(nodeId: NodeId): F[RedisModulesAsyncCommands[K, V]]
   def liftK[G[_]: Async]: RedisConnection[G, K, V]
 }
 
 private[redis4cats] class RedisStatefulConnection[F[_]: ApplicativeThrow: FutureLift: RedisExecutor, K, V](
-    conn: StatefulRedisConnection[K, V]
+    conn: StatefulRedisModulesConnection[K, V]
 ) extends RedisConnection[F, K, V] {
-  def sync: F[RedisSyncCommands[K, V]] = RedisExecutor[F].delay(conn.sync())
-  def clusterSync: F[RedisClusterSyncCommands[K, V]] =
+  def sync: F[RedisModulesSyncCommands[K, V]] = RedisExecutor[F].delay(conn.sync())
+  def clusterSync: F[RedisModulesClusterSyncCommands[K, V]] =
     OperationNotSupported("Running in a single node").raiseError
-  def async: F[RedisAsyncCommands[K, V]] = RedisExecutor[F].delay(conn.async())
-  def clusterAsync: F[RedisClusterAsyncCommands[K, V]] =
+  def async: F[RedisModulesAsyncCommands[K, V]] = RedisExecutor[F].delay(conn.async())
+  def clusterAsync: F[RedisModulesClusterAsyncCommands[K, V]] =
     OperationNotSupported("Running in a single node").raiseError
   def close: F[Unit] = FutureLift[F].liftCompletableFuture(RedisExecutor[F].delay(conn.closeAsync())).void
-  def byNode(nodeId: NodeId): F[RedisAsyncCommands[K, V]] =
+  def byNode(nodeId: NodeId): F[RedisModulesAsyncCommands[K, V]] =
     OperationNotSupported("Running in a single node").raiseError
   def liftK[G[_]: Async]: RedisConnection[G, K, V] = {
     implicit val ecG: RedisExecutor[G] = RedisExecutor[F].liftK[G]
@@ -62,17 +62,17 @@ private[redis4cats] class RedisStatefulConnection[F[_]: ApplicativeThrow: Future
 }
 
 private[redis4cats] class RedisStatefulClusterConnection[F[_]: FutureLift: MonadThrow: RedisExecutor, K, V](
-    conn: StatefulRedisClusterConnection[K, V]
+    conn: StatefulRedisModulesClusterConnection[K, V]
 ) extends RedisConnection[F, K, V] {
-  def sync: F[RedisSyncCommands[K, V]] =
+  def sync: F[RedisModulesSyncCommands[K, V]] =
     OperationNotSupported("Transactions are not supported in a cluster. You must select a single node.").raiseError
-  def async: F[RedisAsyncCommands[K, V]] =
+  def async: F[RedisModulesAsyncCommands[K, V]] =
     OperationNotSupported("Transactions are not supported in a cluster. You must select a single node.").raiseError
-  def clusterAsync: F[RedisClusterAsyncCommands[K, V]] = RedisExecutor[F].delay(conn.async())
-  def clusterSync: F[RedisClusterSyncCommands[K, V]]   = RedisExecutor[F].delay(conn.sync())
+  def clusterAsync: F[RedisModulesClusterAsyncCommands[K, V]] = RedisExecutor[F].delay(conn.async())
+  def clusterSync: F[RedisModulesClusterSyncCommands[K, V]]   = RedisExecutor[F].delay(conn.sync())
   def close: F[Unit] =
     FutureLift[F].liftCompletableFuture(RedisExecutor[F].delay(conn.closeAsync())).void
-  def byNode(nodeId: NodeId): F[RedisAsyncCommands[K, V]] =
+  def byNode(nodeId: NodeId): F[RedisModulesAsyncCommands[K, V]] =
     FutureLift[F].liftCompletableFuture(RedisExecutor[F].delay(conn.getConnectionAsync(nodeId.value))).flatMap {
       stateful => RedisExecutor[F].delay(stateful.async())
     }

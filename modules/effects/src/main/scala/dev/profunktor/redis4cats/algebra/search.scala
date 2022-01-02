cbb95392a088d451e5d4957159e5b30451cab5c1
varygin <varygin@arrival.com>
2022-01-02 18:00:45 +0500

    Make SearchCommands API (still have questions about return types)

--- modules/effects/src/main/scala/dev/profunktor/redis4cats/algebra/search.scala
+++ modules/effects/src/main/scala/dev/profunktor/redis4cats/algebra/search.scala
@@ -2,29 +2,24 @@ package dev.profunktor.redis4cats.algebra
 
 import com.redis.lettucemod.search.{AggregateOptions, AggregateResults, AggregateWithCursorResults, CreateOptions, Cursor, Field, SearchOptions, SearchResults, SuggetOptions, Suggestion => LettuceSuggestion}
 
-trait SearchCommands[F[_], K, V] extends DDL[F, K, V]
+trait SearchCommands[F[_], K, V]
+  extends Create[F, K, V]
   with Search[F, K, V]
-  with Aggregate[F, K, V]
+  with Update[F, K]
+  with Delete[F, K, V]
+  with Alias[F, K, V]
+  with Tags[F, K, V]
   with Suggestion[F, K, V]
   with Dictionary[F, K, V]
+  with Info[F, K]
 
-trait DDL[F[_], K, V] {
+trait Create[F[_], K, V] {
   def create(index: K, options: Option[CreateOptions[K, V]], fields: Field*): F[Unit]
-
-  def dropIndex(index: K): F[Unit] // TODO boolean?
-
-  def dropIndexDeleteDocs(index: K): F[Unit] // TODO boolean?
-
-  def alter(index: K, field: Field): F[Unit] // TODO what if field already exist?
-
-  //TODO continue
 }
 
 trait Search[F[_], K, V] {
   def search(index: K, query: V, options: Option[SearchOptions[K, V]] = None): F[SearchResults[K, V]]
-}
 
-trait Aggregate[F[_], K, V] {
   def aggregate(index: K, query: V, options: Option[AggregateOptions[K, V]] = None): F[AggregateResults[K]]
 
   def aggregate(index: K, query: V, cursor: Cursor, options: Option[AggregateOptions[K, V]]): F[AggregateWithCursorResults[K]]
@@ -34,8 +29,28 @@ trait Aggregate[F[_], K, V] {
   def cursorRead(index: K, cursor: Long, count: Long): F[AggregateWithCursorResults[K]]
 
   def cursorDelete(index: K, cursor: Long): F[Boolean] //TODO if delete true?
+}
 
-  //TODO tagvals???
+trait Update[F[_], K] {
+  def alter(index: K, field: Field): F[Unit] // TODO what if field already exist?
+}
+
+trait Delete[F[_], K, V] {
+  def dropIndex(index: K): F[Unit] // TODO boolean?
+
+  def dropIndexDeleteDocs(index: K): F[Unit] // TODO boolean?
+}
+
+trait Alias[F[_], K, V] {
+  def aliasAdd(name: K, index: V): F[Boolean]
+
+  def aliasUpdate(name: K, index: V): F[Boolean]
+
+  def aliasDel(name: K): F[Boolean]
+}
+
+trait Tags[F[_], K, V] {
+  def tagVals(index: K, field: K): F[Seq[V]]
 }
 
 trait Suggestion[F[_], K, V] {
@@ -60,4 +75,10 @@ trait Dictionary[F[_], K, V] {
   def dictDel(dict: K, terms: V*): F[Long]
 
   def dictDump(dict: K): F[Seq[V]]
+}
+
+trait Info[F[_], K] {
+  def indexInfo(index: K): F[List[Any]] //TODO looks not very typesafe :(
+
+  def list: F[Seq[K]]
 }
